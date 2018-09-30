d970a176bfc23fa7ed0e288d4cd145ffd5382f02
cheppak <cheppi74@gmail.com>
2018-09-30 00:12:53 -0300

    Cambiamos a Junit

--- moneyxchangebe/src/test/java/ar/com/sac/service/ExchangeRateServiceTestCase.java
+++ moneyxchangebe/src/test/java/ar/com/sac/service/ExchangeRateServiceTestCase.java
@@ -1,20 +1,9 @@
 package ar.com.sac.service;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit4.SpringRunner;
+import junit.framework.TestCase;
 
-import ar.com.sac.repository.ExchangeRateRepository;
+public class ExchangeRateServiceTestCase extends TestCase{
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
-public class ExchangeRateServiceTestCase {
-
-	@MockBean
-	private ExchangeRateRepository repository;
-	
 	public void testFirstTime(){
 		
 	}
